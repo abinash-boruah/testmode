4f83d41dcf382fdfe98d6e2cd80e6ed822706296
Abinash <abinash.boruah09@gmail.com>
2019-02-23 15:11:32 +0530

    table added

--- src/components/TestPage.js
+++ src/components/TestPage.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import Card from './Card'
 import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 const Test = ({ match }) => (
-    <Container>
-        as
-        Test ID:
-      <h2>{ match.params.testid }</h2>
+    <Container fluid={true}>
+      <h3>TestMode Overview</h3>
+      <Row>
+         <Col> <Card header="Total tests" value="34"/> </Col>
+         <Col> <Card header="Average Student Score" value="34"/> </Col>
+         <Col> <Card header="How to use TestMode" value="jkj"/> </Col>
+      </Row>
+    <h2>{ match.params.testid }</h2>
     </Container>
 );
 
--- src/components/TestsPage.js
+++ src/components/TestsPage.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import Container from 'react-bootstrap/Container'
+import Table from 'react-bootstrap/Table'
+import Button from 'react-bootstrap/Button'
 
 
 const Tests = ({ match }) => (
     <Container>
       <h2>Tests</h2>
-      <li><Link to="/test/asdasd">Test 1</Link></li> 
-      <li><Link to="/test/ppp">Test asd1</Link></li> 
-      <li><Link to="/test/asldkljasld">Test asdasd1</Link></li> 
+      <Table  bordered hover size="sm">
+       <thead>
+        <tr>
+         <th>ID</th>
+         <th>Name</th>
+         <th>Status</th>
+         <th>Modify</th>
+        </tr>
+       </thead>
+       <tbody>
+        <tr>
+         <td>1</td>
+         <td><Link to="/test/asdasd">Test 1</Link></td>
+         <td>Ongoing</td>
+         <td><Button variant="primary">Edit</Button> <Button variant="danger">Delete</Button></td>
+        </tr>
+        <tr>
+         <td>2</td>
+         <td><Link to="/test/ppp">Test 2</Link></td>
+         <td>Completed</td>
+         <td><Button variant="primary">Edit</Button> <Button variant="danger">Delete</Button></td> 
+        </tr>
+        <tr>
+         <td>3</td>
+         <td><Link to="/test/asldkljasld">Test 3</Link></td>
+         <td>Upcoming</td>
+         <td><Button variant="primary">Edit</Button> <Button variant="danger">Delete</Button></td> 
+        </tr>
+      </tbody>
+      </Table>;
     </Container>
 );
 
