7242b42714cbc8362d5ef92442f21bd86a331ed8
geekodour <hrishikeshbman@gmail.com>
2019-02-23 12:30:57 +0530

    wip

--- src/components/SideBarLink.js
+++ src/components/SideBarLink.js
@@ -1,6 +1,10 @@
 import React from 'react'
 
+const sideBarLinkStyle = {
+  padding: '0.5em'
+}
+
 const SideBarLink = ({icon, text}) => (
-     <div> <i class={"icon ion-md-"+icon}></i> {text}</div>
+     <div style={sideBarLinkStyle}> <i class={"icon ion-md-"+icon}></i> {text}</div>
 )
 export default SideBarLink
