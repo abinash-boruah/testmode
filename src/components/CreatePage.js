06e27d25c413a08914f55753c26b21f0355c0166
Abinash <abinash.boruah09@gmail.com>
2019-02-25 02:37:39 +0530

    Create Test

--- src/components/CreatePage.js
+++ src/components/CreatePage.js
@@ -7,30 +7,41 @@ import Button from 'react-bootstrap/Button'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import shortid from 'shortid'
+import Dropdown from 'react-bootstrap/Dropdown'
 
 const CreatePage = () => (
     <Container>
       <h2>Create Test</h2>
       <Row>
         <Col xs={3}>
-        <Form>
-  <Form.Group controlId="formBasicEmail">
-    <Form.Label size="sm">Test Name</Form.Label>
-    <Form.Control size="sm" placeholder=" Eg. Class-8-Chapter3-ClassTest" />
-    <Form.Label size="sm">Creation Time</Form.Label>
-    <Form.Control size="sm" disabled value={Date.now()} />
-    <Form.Label size="sm">TestID</Form.Label>
-    <Form.Control size="sm" value={shortid.generate()} />
-  </Form.Group>
-</Form>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label size="sm">Test Name</Form.Label>
+              <Form.Control size="sm" placeholder=" Eg. Class-8-Chapter3-ClassTest" />
+              <Form.Label size="sm" style={{marginTop:"5px"}}>Creation Time</Form.Label>
+              <Form.Control size="sm" disabled value={Date.now()} />
+              <Form.Label size="sm" style={{marginTop:"5px"}}>TestID</Form.Label>
+              <Form.Control size="sm" value={shortid.generate()} />
+           </Form.Group>
+        </Form>
         </Col>
-        <Col>
-        </Col>
+        <Col> 
+          <Form>
+          <Form.Label size ="sm">Question</Form.Label>
+          <Form.Control as="textarea" size="lg" placeholder=" " />      
+          </Form>
+          <Dropdown>
+            <Dropdown.Toggle variant="primary" id="dropdown-basic" style ={{marginTop:"6px"}}>
+              Answer Type
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-2">Text</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Multiple Choice</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+          <Button variant="primary" style={{marginTop:"6px"}}>Add Question</Button><Button variant="success" style={{marginTop:"7px",marginLeft:'70%'}}>Save Test</Button>
+        </Col>     
       </Row>
-
-
-
-      
     </Container>
 );
 export default CreatePage
